52d5b080a361e6e7a935fe288527b5a2ac3f0d24
Maciej Białczak <m.bialczak@protonmail.com>
2023-04-23 18:52:58 +0200

    completed analyze for later work

--- Vector-ut.cpp
+++ Vector-ut.cpp
@@ -9,7 +9,23 @@ namespace my::test {
 using testing::A;
 using testing::An;
 
-TEST(VectorTests, sizeOfVectorShouldBeEqualTo24)
+// NOTE: dummy allocator just for testing. Does not allocate anything
+template <typename Type>
+struct DummyAllocator
+{
+    using size_type = std::size_t;
+
+    [[nodiscard]] static constexpr Type* allocate(size_type n) { }
+    static constexpr void deallocate(Type* ptr) { }
+
+    template <typename... Args>
+    static void construct(Type* ptr, Args&&... args)
+    { }
+
+    static void destroy(Type* ptr) { }
+};
+
+TEST(VectorMemorySizeTest, sizeOfVectorShouldBeEqualTo24)
 {
     Vector<int> sut1;
     Vector<double> sut2;
@@ -21,15 +37,63 @@ TEST(VectorTests, sizeOfVectorShouldBeEqualTo24)
     EXPECT_EQ(sizeof(sut3), requiredSize);
 }
 
-TEST(VectorTests, valueTypeAliasShouldBeDefinedAndPointToUnderlyingType)
+TEST(VectorTypeAliasTest, valueTypeAliasShouldBeDefinedAndPointToUnderlyingType)
 {
-    Vector<int>::value_type shouldBeInt;
-    Vector<double>::value_type shouldBeDouble;
-    Vector<std::string>::value_type shouldBeString;
+    Vector<int>::value_type valueTypeForInt;
+    Vector<double>::value_type valueTypeForDouble;
+    Vector<std::string>::value_type valueTypeForString;
 
-    EXPECT_THAT(shouldBeInt, An<int>());
-    EXPECT_THAT(shouldBeDouble, An<double>());
-    EXPECT_THAT(shouldBeString, An<std::string>());
+    EXPECT_THAT(valueTypeForInt, An<int>());
+    EXPECT_THAT(valueTypeForDouble, A<double>());
+    EXPECT_THAT(valueTypeForString, A<std::string>());
 }
 
+TEST(VectorTypeAliasTest, allocatorTypeAliasShouldBeDefinedAndPointToDefaultAllocatorIfNonePassed)
+{
+    Vector<int>::allocator_type allocatorForInt;
+    Vector<double>::allocator_type allocatorForDouble;
+    Vector<std::string>::allocator_type allocatorForString;
+
+    EXPECT_THAT(allocatorForInt, A<DefaultAllocator<int>>());
+    EXPECT_THAT(allocatorForDouble, A<DefaultAllocator<double>>());
+    EXPECT_THAT(allocatorForString, A<DefaultAllocator<std::string>>());
+}
+
+TEST(VectorTypeAliasTest, allocatorTypeAliasShouldBeDefinedAndPointToExplicitlyPassedAllocator)
+{
+    Vector<int, DummyAllocator<int>>::allocator_type allocatorForInt;
+    Vector<double, DummyAllocator<double>>::allocator_type allocatorForDouble;
+    Vector<std::string, DummyAllocator<std::string>>::allocator_type allocatorForString;
+
+    EXPECT_THAT(allocatorForInt, A<DummyAllocator<int>>());
+    EXPECT_THAT(allocatorForDouble, A<DummyAllocator<double>>());
+    EXPECT_THAT(allocatorForString, A<DummyAllocator<std::string>>());
+}
+
+TEST(VectorTypeAliasTest, sizeTypeAliasShouldBeDefinedAndPointToStdSizeType)
+{
+    Vector<int>::size_type sizeTypeForInt;
+    Vector<double>::size_type sizeTypeForDouble;
+    Vector<std::string>::size_type sizeTypeForString;
+
+    EXPECT_THAT(sizeTypeForInt, A<std::size_t>());
+    EXPECT_THAT(sizeTypeForDouble, A<std::size_t>());
+    EXPECT_THAT(sizeTypeForString, A<std::size_t>());
+}
+
+TEST(VectorTypeAliasTest, iteratorTypeAliasShouldBeDefinedAndMeetExpectations)
+{
+    EXPECT_TRUE(std::random_access_iterator<Vector<int>::iterator>);
+    EXPECT_TRUE(std::contiguous_iterator<Vector<int>::iterator>);
+}
+
+TEST(VectorTypeAliasTest, constIteratorTypeAliasShouldBeDefinedAndMeetExpectations)
+{
+    EXPECT_TRUE(std::random_access_iterator<Vector<int>::const_iterator>);
+    EXPECT_TRUE(std::contiguous_iterator<Vector<int>::const_iterator>);
+
+    // TODO: complete when accessor functions are available
+    //     auto sutConstInter = Vector<int>::const_iterator{}
+    //     EXPECT_TRUE(std::is_const_v<Vector<int>::const_iterator>);
+}
 }   // namespace my::test
--- Vector.hpp
+++ Vector.hpp
@@ -12,8 +12,135 @@ class Vector
     using allocator_type = Allocator;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
-    // TODO: REMOVE
-    void dummyFunction() {};
+    using iterator = Type*;
+    using const_iterator = const Type*;
+
+    // TODO: VERIFY functions to implement
+    //  ============= CONSTRUCTOR ================
+    // constexpr vector() noexcept(noexcept(Allocator()));
+
+    // constexpr explicit vector(const Allocator& alloc) noexcept;
+
+    // constexpr vector(size_type count,
+    //                  const T& value,
+    //                  const Allocator& alloc = Allocator());
+
+    // constexpr explicit vector(size_type count,
+    //                           const Allocator& alloc = Allocator());
+
+    // constexpr vector(InputIt first, InputIt last, const Allocator& alloc = Allocator());
+
+    // constexpr vector(const vector& other);
+
+    // constexpr vector(const vector& other, const Allocator& alloc);
+
+    // constexpr vector(vector&& other) noexcept;
+
+    // constexpr vector(vector&& other, const Allocator& alloc);
+
+    // constexpr vector(std::initializer_list<T> init,
+    //                  const Allocator& alloc = Allocator());
+
+    // ============= DESTRUCTOR ===================
+    // constexpr ~vector();
+    // TODO: VERIFY end
+
+    // ============= ASSIGNMENT OPERATOR ==========
+    // constexpr vector& operator=(const vector& other);
+
+    // constexpr vector& operator=(vector&& other) noexcept;
+
+    // constexpr vector& operator=(std::initializer_list<T> ilist);
+
+    // ============= ASSIGN FUNCTION ==========
+    // constexpr void assign(size_type count, const T& value);
+
+    // template <class InputIt>
+    // constexpr void assign(InputIt first, InputIt last);
+
+    // constexpr void assign( std::initializer_list<T> ilist );
+
+    // ============= get_allocator function ===========
+    // constexpr allocator_type get_allocator() const noexcept;
+
+    // ============= at function =============
+    // constexpr reference at( size_type pos );
+    // constexpr const_reference at( size_type pos ) const;
+
+    // ============= operator[] ==============
+    // constexpr reference operator[](size_type pos);
+
+    // constexpr const_reference operator[]( size_type pos ) const;
+
+    // ============= front function =============
+    // constexpr reference front();
+
+    // constexpr const_reference front() const;
+
+    // ============= back function ==============
+    // constexpr reference back();
+
+    // constexpr const_reference back() const;
+
+    // ============= data function ==============
+    // constexpr T* data() noexcept;
+
+    // constexpr const T* data() const noexcept;
+
+    // // TODO: consider iterator functions like begin(), cbegin, rend,end)()
+    // constexpr iterator begin() noexcept;
+    // constexpr const_iterator begin() const noexcept;
+    // constexpr const_iterator cbegin() const noexcept;
+
+    // constexpr iterator end() noexcept;
+    // constexpr const_iterator end() const noexcept;
+    // constexpr const_iterator cend() const noexcept;
+
+    // constexpr reverse_iterator rbegin() noexcept;
+    // constexpr const_reverse_iterator rbegin() const noexcept;
+    // constexpr const_reverse_iterator crbegin() const noexcept;
+
+    // constexpr reverse_iterator rend() noexcept;
+    // constexpr const_reverse_iterator rend() const noexcept;
+    // constexpr const_reverse_iterator crend() const noexcept;
+
+    // ============== capacity functions =============
+    // [[nodiscard]] constexpr bool empty() const noexcept;
+    // constexpr size_type size() const noexcept;
+    // constexpr size_type max_size() const noexcept;
+    // constexpr void reserve(size_type new_cap);
+    // constexpr size_type capacity() const noexcept;
+    // constexpr void shrink_to_fit();
+    // // ============== modifiers ======================
+    // constexpr void clear() noexcept;
+
+    // constexpr iterator insert(const_iterator pos, const T& value);
+    // constexpr iterator insert(const_iterator pos, T&& value);
+    // constexpr iterator insert(const_iterator pos, size_type count, const T& value);
+    // template <class InputIt>
+    // constexpr iterator insert(const_iterator pos, InputIt first, InputIt last);
+    // constexpr iterator insert(const_iterator pos, std::initializer_list<T> ilist);
+
+    // template <class... Args>
+    // constexpr iterator emplace(const_iterator pos, Args&&... args);
+
+    // constexpr iterator erase(const_iterator pos);
+    // constexpr iterator erase(const_iterator first, const_iterator last);
+
+    // constexpr void push_back(const T& value);
+    // constexpr void push_back(T&& value);
+    // template <class... Args>
+    // constexpr reference emplace_back(Args&&... args);
+
+    // constexpr void pop_back();
+
+    // constexpr void resize(size_type count);
+    // constexpr void resize(size_type count, const value_type& value);
+    // constexpr void swap( vector& other ) noexcept;
+
+    // template <class T, class Alloc>
+    // constexpr operator<=>(const std::vector<T, Alloc>& lhs,
+    //                       const std::vector<T, Alloc>& rhs);
 
   private:
     // TODO: VERIFY
@@ -23,7 +150,18 @@ class Vector
     static DefaultAllocator<Type> allocator;   // TODO: VERIFY name and sense of storing
 };
 
-template <typename Type, typename Allocator>
-DefaultAllocator<Type> Vector<Type, Allocator>::allocator;
+// template <typename Type, typename Allocator>
+// DefaultAllocator<Type> Vector<Type, Allocator>::allocator;
+
+// TODO:
+// =========== specialization for std::swap
+// constexpr void swap(std::vector<T, Alloc>& lhs,
+//                     std::vector<T, Alloc>& rhs) noexcept;
+// =========== erase function
+// constexpr typename std::vector<T, Alloc>::size_type
+//     erase(std::vector<T, Alloc>& c, const U& value);
+
+// constexpr typename std::vector<T, Alloc>::size_type
+//     erase_if(std::vector<T, Alloc>& c, Pred pred);
 
 }   // namespace my
